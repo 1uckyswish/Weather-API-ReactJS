7c476bb41f1f89d5ae9e35faea87de0fc5cd745c
Noel Guillen <noelblas274@gmail.com>
2023-07-18 20:41:57 -0400

    data passed a props

--- src/Componets/WeatherCard.jsx
+++ src/Componets/WeatherCard.jsx
@@ -1,5 +1,6 @@
-import {useState} from 'react'
-import {useDate} from "../Utils/useDate"
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react'
+import { useDate } from '../Utils/useDate'
 import sun from '../assets/icons/sun.png'
 import cloud from '../assets/icons/cloud.png'
 import fog from '../assets/icons/fog.png'
@@ -9,8 +10,7 @@ import storm from '../assets/icons/storm.png'
 import wind from '../assets/icons/windy.png'
 import '../index.css'
 
-
-function WeatherCard({
+const WeatherCard = ({
   temperature,
   windspeed,
   humidity,
@@ -18,12 +18,14 @@ function WeatherCard({
   heatIndex,
   iconString,
   conditions,
-}) {
+}) => {
 
-  const [icon, setIcon] = useState(sun);
-  const { time } = useDate();
+  const [icon, setIcon] = useState(sun)
+  const { time } = useDate()
+  const celsius = temperature;
+  const fahrenheit = Math.round((celsius * 9/5) + 32);
 
-    useEffect(() => {
+  useEffect(() => {
     if (iconString) {
       if (iconString.toLowerCase().includes('cloud')) {
         setIcon(cloud)
@@ -47,7 +49,27 @@ function WeatherCard({
     <div className='w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4'>
       <div className='flex w-full just-center, items-center gap-4 mt-12 mb-4'>
         <img src={icon} alt="weather_icon" />
-        <p className='font-bold text-5xl flex justify-center items-center' >{temperature} &deg;C</p>
+        <p className='font-bold text-5xl flex justify-center items-center' >{fahrenheit.toFixed(0)} &deg;F</p>
+      </div>
+      <div className='font-bold text-center text-xl'>
+        {place}
+      </div>
+      <div className='w-full flex justify-between items-center mt-4'>
+        <p className='flex-1 text-center p-2'>{new Date().toDateString()}</p>
+        <p className='flex-1 text-center p-2'>{time}</p>
+      </div>
+      <div className='w-full flex justify-between items-center mt-4 gap-4'>
+        <p className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
+        <p className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>Humidity <p className='font-normal'>{humidity} gm/m&#179;</p></p>
+      </div>
+      <div className='w-full p-3 mt-4 flex justify-between items-center'>
+        <p className='font-mono text-lg'>Heat Index</p>
+        <p className='text-xl font-semibold'>{heatIndex ? heatIndex : 'N/A'}</p>
+      </div>
+      <hr className='bg-slate-600' />
+      <div className='w-full p-3 mt-4 flex justify-between items-center'>
+        <p className='font-mono text-lg'>Conditions</p>
+        <p className='text-xl font-semibold'>{conditions}</p>
       </div>
     </div>
   )
